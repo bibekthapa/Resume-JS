826ee8314c72084f90db7b536efd8cac64152a8f
Bibek <bibek.thapahere@gmail.com>
2016-10-23 02:43:10 -0700

    initial commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,20 +4,8 @@ This is empty on purpose! Your code to build the resume will go here.
 
 var formattedName="Bibek Thapa";
 var formattedRole="Web Developer";
-var phoneNumber="9860559853";
-var skills=["Android Programming","Java Programming","JS"];
-var welcomeMessage="Hi I am "+" "+formattedName;
-var bio={
-	"name":formattedName,
-	"role":formattedRole,
-	"pictureUrl":"images/me.jpeg",
-	"contactInfo":phoneNumber,
-	"skills":skills
-
-};
 
 
-$("#header").append(HTMLheaderName.replace("%data%",bio.name)); //HTMLheaderNameis in the helper.js class
-$("#header").append(HTMLheaderRole.replace("%data%",bio.skills));
-
+$("#header").append(HTMLheaderName.replace("%data%",formattedName));
+$("#header").prepend(HTMLheaderRole.replace("%data%",formattedRole));
 
